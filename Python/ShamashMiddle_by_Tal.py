0f9ad97900962d799ba55cde82fc949be81c4b6d
Shani <shanigeva12@gmail.com>
2021-11-29 18:37:08 +0200

    added py solv

---
+++ Python/ShamashMiddle_by_Tal.py
@@ -0,0 +1,20 @@
+def menora(ner):
+    ner = int(input("Enter today's ner number: "))
+    candles = ner + 1
+    return("For today's Ner you will need %d candles")%(candles)
+print(menora(2))
+def paint_menora(ner):
+    ner = int(input("Enter today's ner number: "))
+    candle = "|"
+    shamash = "!"
+    menora = ""
+    if ner < 5:
+        for i in range(ner):
+            menora += candle
+        return menora + shamash
+    else:
+        menora += candle*4 + shamash
+        for i in range(ner-4):
+            menora += candle
+        return menora
+print(paint_menora(8))
